792ed223eb8e0f699e77b315ecc274f2aa139cce
m0 <m0@localhost>
2008-12-04 02:55:34 +0000

    installer update.

--- insthier.c
+++ insthier.c
@@ -17,6 +17,6 @@ struct install_item insthier[] = {
   {INST_COPY, "sdl-ttf-ada.sld", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "sdl-ttf-ada.a", "libsdl-ttf-ada.a", ctxt_slibdir, 0, 0, 0644},
   {INST_COPY, "sdl-ttf-ada-conf.ld", 0, ctxt_repos, 0, 0, 0644},
-  {INST_COPY, "sdl-ttf-ada-conf", 0, ctxt_bindir, 0, 0, 0755},
+  {INST_COPY_EXEC, "sdl-ttf-ada-conf", 0, ctxt_bindir, 0, 0, 0755},
 };
 unsigned long insthier_len = sizeof(insthier) / sizeof(struct install_item);
